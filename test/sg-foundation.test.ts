cdefa480bf37cdaaedaaf32fe964738fd0e0fb37
cmainhightower <151663524+cmainhightower@users.noreply.github.com>
2025-04-04 12:58:30 -0500

    cdk init

---
+++ test/sg-foundation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SgFoundation from '../lib/sg-foundation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sg-foundation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SgFoundation.SgFoundationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
